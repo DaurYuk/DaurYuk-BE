8a311adaebd8415f97de2ad73eb63d6e38680296
calvinrev20 <111202113782@dinus.ac.id>
2024-06-02 22:34:12 +0700

    Menambahkan  GET profile Information

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,6 +11,7 @@ const SignupController = require('../controllers/user/signup');
 const VerifyAccountController = require('../controllers/user/verify_account');
 const LoginController = require('../controllers/user/login');
 
+
 // JSON Body Validation Schemas
 const userCredentialSchema = require('../utils/schemas/UserCredentialSchema');
 
@@ -26,5 +27,24 @@ app.get('/ping', ping);
 app.post('/register', checkSchema(userCredentialSchema), SignupController);
 app.get('/verify-account', query('token').notEmpty(), VerifyAccountController);
 app.post('/login', checkSchema(userCredentialSchema), LoginController);
+
+// Get Profile Information
+exports.getProfileInformation = async (req, res) => {
+    try {
+      const { id } = req.token;
+      const user = await getUserDataFromFirestore(id);
+  
+      if (!user) {
+        return sendError(res, 404, 'User not found');
+      }
+  
+      // Remove sensitive information from user object before sending it to the client
+      const { password, ...userProfile } = user;
+  
+      return sendSuccess(res, 200, userProfile);
+    } catch (error) {
+      return sendError(res, 500, 'Internal server error');
+    }
+  };
 
 module.exports = app;
